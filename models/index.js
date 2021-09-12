bae82686193fb1ad2a2f2943c6ad3a91baaf4f65
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-09-12 12:31:22 +1000

    added action model files

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User,{
+    foreignKey:'userId',
+    onDelete:'cascade'
+});
+
+Post.hasMany(Comment,{
+    foreignKey:'postId',
+    onDelete:'cascade'
+});
+
+Comment.belongsTo(User,{
+    foreignKey:'userId',
+    onDelete:'cascade'
+
+});
+
+module.exports ={
+    User,
+    Comment,
+    Post,
+   
+};
