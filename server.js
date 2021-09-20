dc7af877c94b1f0cff0579c8a6d3865a247337eb
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-09-20 18:12:12 +1000

    modified server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const sess = {
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(session(sess));
+app.use(session({ secret: 'somevalue' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
